1eaed25866720a0905ef0ca7b74d0d8a6158f9db
chenruochen <chenruochen@qudian.com>
2020-12-26 16:42:21 +0800

    新增 import方法

--- import_invoice.py
+++ import_invoice.py
@@ -6,6 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_manager.settings')
 django.setup()
 
 from invoice_manager.vat_invoice import invoice
+from invoice_manager.job import invoice_from_email_job
 
 if __name__ == '__main__':
-    invoice()
+    invoice_from_email_job()
--- wechat/robot.py
+++ wechat/robot.py
@@ -10,9 +10,14 @@ robot = WeRoBot(enable_session=False,
                 ENCODING_AES_KEY='zTPfDoFmMdXpMazPp20Rw2e086MJIZ3EQQHli4BjkI3')
 
 from invoice_manager.job import invoice_from_email_job
-
+from invoice_manager.vat_invoice import invoice
 
 @robot.filter("job")
 def hello(message):
     unread_count = invoice_from_email_job()
     return '未读邮件' + str(unread_count)
+
+@robot.filter("import")
+def import_invoice(message):
+    invoice()
+    return '执行成功'
